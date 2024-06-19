524a8542f1a8ea03ffe2d666c51522798d951ecd
Alerechi Ordu <alerechiordu@gmail.com>
2024-06-19 23:18:25 +0100

    services added successfully

--- Repository/Services/QuestionService.cs
+++ Repository/Services/QuestionService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Azure.Cosmos;
 using Newtonsoft.Json;
 using ProgramAplicationAPI.Core.Dtos;
+using ProgramAplicationAPI.Core.Model;
 using ProgramAplicationAPI.Repository;
 using ProgramAplicationAPI.Repository.Interface;
 
@@ -8,38 +9,85 @@ namespace ProgramAplicationAPI.Repository.Services
 {
     public class QuestionService : IQuestionService
     {
-        private readonly Container _questionContainer;
-        //private readonly Microsoft.Azure.Cosmos.Container _questionContainer;
+        private readonly Container _container;
+        private readonly CosmosClient _cosmosClient;
+        //private readonly Microsoft.Azure.Cosmos.Container _container;
 
 
         public QuestionService(CosmosClient cosmosClient, string databaseName, string containerName )
         {
-            _questionContainer = cosmosClient.GetContainer(databaseName, containerName);
+            _cosmosClient = cosmosClient;
+            _container = cosmosClient.GetContainer(databaseName, containerName);
         }
 
-        public async Task<QuestionDto> CreateQuestionAsync(QuestionDto question)
+        public async Task<QuestionDto> CreateQuestionAsync(QuestionDto questionDto)
         {
-            throw new NotImplementedException();
+            var questionModel = MapQuestionDtoToModel(questionDto);
+            await _container.CreateItemAsync(questionModel);
+            return questionDto;
         }
 
         public async Task DeleteQuestionAsync(string id)
         {
-            throw new NotImplementedException();
+            await _container.DeleteItemAsync<QuestionModel>(id, new PartitionKey(id));
         }
 
         public async Task<QuestionDto> GetQuestionAsync(string id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var question = await _container.ReadItemAsync<QuestionModel>(id, new PartitionKey(id));
+                return MapQuestionModelToDto(question);
+            }
+            catch (CosmosException ex)
+            {
+                throw new Exception($"Error getting question: {ex.Message}");
+            }
         }
 
         public async Task<List<QuestionDto>> GetQuestionsAsync()
         {
-            throw new NotImplementedException();
+            var questions = new List<QuestionModel>();
+            var iterator = _container.GetItemQueryIterator<QuestionModel>();
+            while (iterator.HasMoreResults)
+            {
+                var questionModel = await iterator.ReadNextAsync();
+                questions.AddRange(questionModel);
+            }
+            return questions.Select(MapQuestionModelToDto).ToList();
+        }
+        public async Task<QuestionDto> UpdateQuestionAsync(QuestionDto questionDto)
+        {
+            var questionModel = MapQuestionDtoToModel(questionDto);
+            await _container.UpsertItemAsync(questionModel);
+            return questionDto;
         }
 
-        public async Task<QuestionDto> UpdateQuestionAsync(QuestionDto question)
+        private QuestionDto MapQuestionModelToDto(QuestionModel questionModel)
         {
-            throw new NotImplementedException();
+            return new QuestionDto
+            {
+                QuestionText = questionModel.QuestionText,
+                QuestionType = questionModel.QuestionType,
+                IsRequired = questionModel.IsRequired,
+                IsInternal = questionModel.IsInternal,
+                DataType = questionModel.DataType,
+                Choices = questionModel.Choices.Select(c => new ChoiceDto { ChoiceText = c.ChoiceText }).ToList()
+            };
+        }
+
+        private QuestionModel MapQuestionDtoToModel(QuestionDto questionDto)
+        {
+            return new QuestionModel
+            {
+                //id = questionDto.id,
+                QuestionText = questionDto.QuestionText,
+                QuestionType = questionDto.QuestionType,
+                IsRequired = questionDto.IsRequired,
+                IsInternal = questionDto.IsInternal,
+                DataType = questionDto.DataType,
+                Choices = questionDto.Choices.Select(c => new ChoiceModel { ChoiceText = c.ChoiceText }).ToList()
+            };
         }
     }
 }

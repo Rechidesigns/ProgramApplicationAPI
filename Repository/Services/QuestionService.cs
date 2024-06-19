fc7f811babdca0d987fe0cc161fd61fb925810a2
Alerechi Ordu <alerechiordu@gmail.com>
2024-06-19 22:58:40 +0100

    added sample data to service and configured the dependency injection

---
+++ Repository/Services/QuestionService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Azure.Cosmos;
+using Newtonsoft.Json;
+using ProgramAplicationAPI.Core.Dtos;
+using ProgramAplicationAPI.Repository;
+using ProgramAplicationAPI.Repository.Interface;
+
+namespace ProgramAplicationAPI.Repository.Services
+{
+    public class QuestionService : IQuestionService
+    {
+        private readonly Container _questionContainer;
+        //private readonly Microsoft.Azure.Cosmos.Container _questionContainer;
+
+
+        public QuestionService(CosmosClient cosmosClient, string databaseName, string containerName )
+        {
+            _questionContainer = cosmosClient.GetContainer(databaseName, containerName);
+        }
+
+        public async Task<QuestionDto> CreateQuestionAsync(QuestionDto question)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteQuestionAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<QuestionDto> GetQuestionAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<QuestionDto>> GetQuestionsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<QuestionDto> UpdateQuestionAsync(QuestionDto question)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
